7ecce7fedf28bada5f8ca522333eb25c5219182f
keiran beaton <keiran beaton>
2016-11-01 14:36:47 -0700

    added profile folder

---
+++ app/components/profile/profile-directive.js
@@ -0,0 +1,17 @@
+'use strict';
+
+module.exports = (app) => {
+  app.directive('kbProfile', function() {
+    return {
+      controller: 'ProfileController',
+      controllerAs: 'profCtrl',
+      template: require('./profile-template.html'),
+      bindToController: true,
+      scope: {
+        baseUrl: '@',
+        config: '@',
+        industryList: '@'
+      }
+    };
+  });
+};
