e4f73d382f7cfe60fdf63e4edd69842d5eacff13
keiran beaton <keiran beaton>
2016-12-06 21:04:49 -0800

    getting search function up and running. info is displaying, need to figure out how to sort

--- app/components/search/user-tab/user-tab-controller.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-module.exports = function(app) {
-  app.controller('UserTabController', ['$scope', UserTabController]);
-  function UserTabController($scope) {
-    this.user = $scope.user;
-    this.image = require('../../assets/' + this.user.image.imageUrl);
-  }
-};
